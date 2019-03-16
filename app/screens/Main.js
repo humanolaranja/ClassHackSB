a4890296e8bf585f4b9afc72ae829549004bd866
Humano Laranja <contato.carlos@outlook.com>
2019-03-16 15:00:16 -0300

    Cards

--- app/screens/Main.js
+++ app/screens/Main.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Container, Header, Title, Button, Left, Right, Body, Icon, Content, Text, Footer, FooterTab } from 'native-base';
+import { Image } from 'react-native';
+import { Container, Header, Title, Button, Left, Right, Body, Icon, Content, Text, Footer, FooterTab, Card, CardItem, } from 'native-base';
 import axios from 'axios';
 
 export default class Main extends React.Component {
@@ -32,7 +33,34 @@ export default class Main extends React.Component {
           <Right />
         </Header>
         <Content>
-         <Text> Oi </Text>
+          {this.state.events.map((event, i) =>
+            <Card style={{flex: 0}} key={i}>
+              <CardItem>
+                <Left>
+                  <Body>
+                    <Text>{event.title.rendered}</Text>
+                    <Text note>{event._event_start}</Text>
+                  </Body>
+                </Left>
+              </CardItem>
+              <CardItem>
+                <Body>
+                  <Image source={{uri: 'https://www.omelhordaculturasp.com/wp-content/uploads/2016/04/maxresdefault.jpg'}} style={{height: 200, width: 350, flex: 1}}/>
+                  <Text>
+                    {event.excerpt.rendered}
+                  </Text>
+                </Body>
+              </CardItem>
+              <CardItem>
+                <Left>
+                  <Button transparent textStyle={{color: '#87838B'}}>
+                    <Icon name="logo-github" />
+                    <Text>1,926 stars</Text>
+                  </Button>
+                </Left>
+              </CardItem>
+            </Card>
+          )}
         </Content>
         <Footer>
           <FooterTab>
